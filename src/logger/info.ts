89e999c3f14ee1de55cadb465639b95d9d70834d
Anthony Yarbrough <asyarb@hawaii.edu>
2022-02-16 00:10:54 -1000

    feat: log is dev

--- src/logger/info.ts
+++ src/logger/info.ts
@@ -1,5 +1,5 @@
 import kleur from "kleur"
 
-export function info(...messages: string[]): void {
+export function info(...messages: unknown[]): void {
   console.info(kleur.bold().blue("[INFO]: " + messages))
 }
