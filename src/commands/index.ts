34035e5368966b823da8dad3803336d3bb1b68af
Anthony Yarbrough <asyarb@hawaii.edu>
2022-07-14 23:19:04 -1000

    fix: directly instantiate in map constructor

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,10 +1,12 @@
 import add from "./add.js"
 import clear from "./clear.js"
-import _delete from "./delete.js"
+import del from "./delete.js"
 import list from "./list.js"
 import { Command } from "../discord/command.js"
 
-const _commands = [add, clear, _delete, list]
-
-export const commands = new Map<string, Command>()
-_commands.forEach((c) => commands.set(c.name, c))
+export const commands = new Map<string, Command>([
+  [add.name, add],
+  [clear.name, clear],
+  [del.name, del],
+  [list.name, list],
+])
