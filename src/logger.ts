2a907cd0ab6378a51a7592940cf67cb8f711ce3c
Anthony Yarbrough <asyarb@hawaii.edu>
2022-07-14 23:44:03 -1000

    fix: spacing

--- src/logger.ts
+++ src/logger.ts
@@ -3,17 +3,17 @@ import kleur from "kleur"
 kleur.enabled = true
 
 export function info(...messages: unknown[]): void {
-  console.info(`${kleur.bold().cyan("[INFO]")}:  ${messages}`)
+  console.info(`${kleur.bold().cyan("[INFO]")}: ${messages}`)
 }
 
 export function warn(...messages: unknown[]): void {
-  console.warn(`${kleur.bold().yellow("[WARN]")}:  ${messages}`)
+  console.warn(`${kleur.bold().yellow("[WARN]")}: ${messages}`)
 }
 
 export function error(...messages: unknown[]): void {
-  console.error(`${kleur.bold().red("[ERROR]")}:  ${messages}`)
+  console.error(`${kleur.bold().red("[ERROR]")}: ${messages}`)
 }
 
 export function debug(...messages: unknown[]): void {
-  console.debug(`${kleur.bold().magenta("[DEBUG]")}:  ${messages}`)
+  console.debug(`${kleur.bold().magenta("[DEBUG]")}: ${messages}`)
 }
